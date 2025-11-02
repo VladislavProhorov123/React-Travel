1124ca06463d75b4c9eb6a33e6da1b67aa931b0b
Vladislav <vladislavprokhorov1005@gmail.com>
2025-11-02 15:11:42 +0200

    title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Components/Hero/Hero'
+import Title from './Components/Title/Title'
 
 export default function App() {
   return (
     <div className=''>
       <Hero />
+      <Title subtitle='CATEGORY' title='We Offer Best Services' /> 
     </div>
   )
 }
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Title.css'
+
+export default function Title({ subtitle, title}) {
+  return (
+    <div className='container'>
+        <h3 className='subtitle'>{subtitle}</h3>
+        <h2 className='title'>{title}</h2>
+    </div>
+  )
+}
